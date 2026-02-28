aedeb8630821c356af70d40b59ee5157077fec74
Rutik Misal <rutikmisal2004@gmail.com>
2026-02-28 21:25:45 +0530

    Add files via upload

---
+++ Input_Array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Input_Array
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int num[]=new int[size];
+		
+		//input
+		for(int i=0;i<size;i++)
+		{
+			num[i]=sc.nextInt();
+		}
+		for(int i=0;i<size;i++)
+		{
+			System.out.println(num[i]);
+		}
+	}
+}
